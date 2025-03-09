b7fd9f54dbbac2f2b7da5944ee3a66d73ef89d06
CruzAntonioDIWWEB <146769717+CruzAntonioDIWWEB@users.noreply.github.com>
2025-03-09 17:29:11 +0100

    arreglo de la estructura de directorios

--- Tarea_6/vite.config.js
+++ Tarea_6/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [],
-  css: {
-    postcss: './postcss.config.js',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/html/index.html'),
+        jquery: resolve(__dirname, 'src/html/jquery.html')
+      }
+    }
   },
+  server: {
+    open: 'src/html/index.html'  //Abre el archivo index.html al iniciar el servidor
+  }
 })
